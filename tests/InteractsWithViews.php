05280032a66e1d37b7be3d1a8f3b6f381ac6c128
driesvints <driesvints@users.noreply.github.com>
2020-05-14 11:51:46 +0000

    Fix styling

--- tests/InteractsWithViews.php
+++ tests/InteractsWithViews.php
@@ -35,7 +35,7 @@ trait InteractsWithViews
             ViewFacade::addLocation(sys_get_temp_dir());
         }
 
-        $tempFile = tempnam($tempDirectory, 'laravel-blade').'.blade.php';
+        $tempFile = tempnam($tempDirectory, 'laravel-blade') . '.blade.php';
 
         file_put_contents($tempFile, $template);
 
