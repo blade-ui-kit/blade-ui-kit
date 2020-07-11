49f30f7a2f3f34565f93192d4c5069ce058e864a
Dries Vints <dries@vints.io>
2020-07-11 16:33:08 +0200

    Cleanup

--- resources/views/components/maps/mapbox.blade.php
+++ resources/views/components/maps/mapbox.blade.php
@@ -1,10 +1,11 @@
 <div id="{{ $id }}" {{ $attributes }}></div>
+
 <script>
     window.onload = function () {
         mapboxgl.accessToken = '{{ config('services.mapbox.public_token') }}';
         var map = new mapboxgl.Map(@json($options()));
 
-        @foreach($markers as $marker)
+        @foreach ($markers as $marker)
             new mapboxgl.Marker()
                 .setLngLat(@json($marker))
                 .addTo(map);
