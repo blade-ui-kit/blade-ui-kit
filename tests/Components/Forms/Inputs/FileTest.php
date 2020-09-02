f42976a8a40ed6e7b1089f72cfb9feb3eab0c03f
A7madXatab <Ahmadzendane725@gmail.com>
2020-09-02 16:57:46 -0700

    adding and registering File input component

---
+++ tests/Components/Forms/Inputs/FileTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Components\Forms\Inputs;
+
+use Tests\Components\ComponentTestCase;
+
+class FileTest extends ComponentTestCase
+{
+    /** @test */
+    public function the_component_can_be_rendered()
+    {
+
+        $expected = <<<HTML
+<div>
+<div>
+    <label for="file" style=" padding: .5rem; border-radius: 5px;" class="">
+    Choose File </label>
+    <span id="chosenFile" class="">
+    </span>
+    
+    <input type="file" name="user_resume" id="file" style="display: none;">
+</div>
+<style>
+    label:hover { color: #718096; }
+</style>
+<script>
+    document.getElementById('file').onchange = function () {
+        document.getElementById('chosenFile').innerHTML = this.files[0].name;
+    };
+</script>
+HTML;
+
+
+        $this->assertComponentRenders(
+            $expected,
+            '<x-file/>'
+        );
+    }
+
+    /** @test */
+    public function specific_attributes_can_be_overwritten()
+    {
+        $expected = <<<HTML
+<div>
+    <label for="file" style=" padding: .5rem; border-radius: 5px;" class="">
+    Choose Resume </label>
+    <span id="chosenFile" class="">
+    </span>
+    
+    <input type="file" name="user_resume" id="file" style="display: none;">
+</div>
+<style>
+    label:hover { color: #718096; }
+</style>
+<script>
+    document.getElementById('file').onchange = function () {
+        document.getElementById('chosenFile').innerHTML = this.files[0].name;
+    };
+</script>
+HTML;
+
+
+        $this->assertComponentRenders(
+            $expected,
+            '<x-file name="user_resume" label="Choose Resume" />'
+        );
+    }
+}
