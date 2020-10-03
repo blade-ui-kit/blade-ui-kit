890e4aec720382e08dc0be3ed714ecccdebee2dd
RomainMazB <romainmazb@gmail.com>
2020-10-04 00:57:31 +0200

    Fix the order of alphabetical ordered list of components

--- config/blade-ui-kit.php
+++ config/blade-ui-kit.php
@@ -17,32 +17,32 @@ return [
 
     'components' => [
         'alert' => Components\Alerts\Alert::class,
-        'form-button' => Components\Buttons\FormButton::class,
-        'logout' => Components\Buttons\Logout::class,
+        'avatar' => Components\Support\Avatar::class,
         'carbon' => Components\DateTime\Carbon::class,
-        'countdown' => Components\DateTime\Countdown::class,
-        'easy-mde' => Components\Editors\EasyMDE::class,
-        'trix' => Components\Editors\Trix::class,
-        'error' => Components\Forms\Error::class,
-        'form' => Components\Forms\Form::class,
-        'label' => Components\Forms\Label::class,
-        'input' => Components\Forms\Inputs\Input::class,
         'checkbox' => Components\Forms\Inputs\Checkbox::class,
         'color-picker' => Components\Forms\Inputs\ColorPicker::class,
+        'countdown' => Components\DateTime\Countdown::class,
+        'cron' => Components\Support\Cron::class,
+        'dropdown' => Components\Navigation\Dropdown::class,
+        'easy-mde' => Components\Editors\EasyMDE::class,
         'email' => Components\Forms\Inputs\Email::class,
-        'password' => Components\Forms\Inputs\Password::class,
-        'pikaday' => Components\Forms\Inputs\Pikaday::class,
-        'textarea' => Components\Forms\Inputs\Textarea::class,
+        'error' => Components\Forms\Error::class,
+        'form' => Components\Forms\Form::class,
+        'form-button' => Components\Buttons\FormButton::class,
         'html' => Components\Layouts\Html::class,
-        'social-meta' => Components\Layouts\SocialMeta::class,
+        'input' => Components\Forms\Inputs\Input::class,
+        'label' => Components\Forms\Label::class,
+        'logout' => Components\Buttons\Logout::class,
         'mapbox' => Components\Maps\Mapbox::class,
         'markdown' => Components\Markdown\Markdown::class,
-        'toc' => Components\Markdown\ToC::class,
-        'dropdown' => Components\Navigation\Dropdown::class,
-        'avatar' => Components\Support\Avatar::class,
-        'cron' => Components\Support\Cron::class,
-        'unsplash' => Components\Support\Unsplash::class,
+        'password' => Components\Forms\Inputs\Password::class,
+        'pikaday' => Components\Forms\Inputs\Pikaday::class,
         'quill' => Components\Editors\Quill::class,
+        'social-meta' => Components\Layouts\SocialMeta::class,
+        'textarea' => Components\Forms\Inputs\Textarea::class,
+        'toc' => Components\Markdown\ToC::class,
+        'trix' => Components\Editors\Trix::class,
+        'unsplash' => Components\Support\Unsplash::class,
     ],
 
     /*
@@ -96,9 +96,19 @@ return [
             'https://unpkg.com/easymde/dist/easymde.min.js',
         ],
 
+        'mapbox' => [
+            'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css',
+            'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js',
+        ],
+
         'moment' => [
             'https://cdn.jsdelivr.net/npm/moment@2.26.0/moment.min.js',
             'https://cdn.jsdelivr.net/npm/moment-timezone@0.5.31/builds/moment-timezone-with-data.min.js',
+        ],
+
+        'pickr' => [
+            'https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css',
+            'https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js',
         ],
 
         'pikaday' => [
@@ -109,16 +119,6 @@ return [
         'trix' => [
             'https://unpkg.com/trix@1.2.3/dist/trix.css',
             'https://unpkg.com/trix@1.2.3/dist/trix.js',
-        ],
-
-        'pickr' => [
-            'https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css',
-            'https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js',
-        ],
-
-        'mapbox' => [
-            'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css',
-            'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js',
         ],
 
         'quill' => [
