05adfae6263d373bb9229566bd52927594559dee
Cyrill N. Kalita <nick@frictionlesssolutions.com>
2021-11-23 15:53:35 -0600

    Add ability to provide $slot to a `label` component

--- resources/views/components/forms/label.blade.php
+++ resources/views/components/forms/label.blade.php
@@ -1,3 +1,3 @@
 <label for="{{ $for }}" {{ $attributes }}>
-    {{ $fallback }}
+    {{ $slot->isEmpty() ? $fallback : $slot }}
 </label>
--- tests/Components/Forms/LabelTest.php
+++ tests/Components/Forms/LabelTest.php
@@ -19,4 +19,16 @@ class LabelTest extends ComponentTestCase
 
         $this->assertComponentRenders($expected, '<x-label for="first_name"/>');
     }
+
+    /** @test */
+    public function the_component_can_be_rendered_with_arbitrary_slot()
+    {
+        $expected = <<<'HTML'
+            <label for="first_name">
+                Foo Name
+            </label>
+            HTML;
+
+        $this->assertComponentRenders($expected, '<x-label for="first_name">Foo Name</x-label>');
+    }
 }
