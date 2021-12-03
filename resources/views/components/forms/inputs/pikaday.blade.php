c07b8679003baa562a3bc99777884b459d54b528
Ryan Chandler <ryangjchandler@gmail.com>
2021-12-03 14:51:41 +0000

    fix: prefer $root over $el in Pikaday

--- resources/views/components/forms/inputs/pikaday.blade.php
+++ resources/views/components/forms/inputs/pikaday.blade.php
@@ -1,10 +1,10 @@
 <input
     x-data
-    x-init="new Pikaday({ field: $el {{ $jsonOptions() }} })"
+    x-init="new Pikaday({ field: $root {{ $jsonOptions() }} })"
     name="{{ $name }}"
     type="text"
     id="{{ $id }}"
     placeholder="{{ $placeholder }}"
-    @if($value)value="{{ $value }}"@endif
+    @if($value) value="{{ $value }}" @endif
     {{ $attributes }}
 />
