efd7000a81c1c5238c1da7749b9549018b201c3a
Jeremias Wolff <comes@users.noreply.github.com>
2022-02-06 09:14:31 +0100

    update tests

--- tests/Components/Forms/Inputs/FlatPickrTest.php
+++ tests/Components/Forms/Inputs/FlatPickrTest.php
@@ -12,7 +12,7 @@ class FlatPickrTest extends ComponentTestCase
     public function the_component_can_be_rendered()
     {
         $expected = <<<'HTML'
-            <input x-data x-init="flatpickr($el, {&quot;dateFormat&quot;:&quot;Y-m-d H:i&quot;,&quot;altInput&quot;:true,&quot;enableTime&quot;:true})" name="birthday" type="text" id="birthday" placeholder="Y-m-d H:i" />
+            <input x-data="{picker:null}" x-on:click="picker=picker||flatpickr($el, {&quot;dateFormat&quot;:&quot;Y-m-d H:i&quot;,&quot;altInput&quot;:true,&quot;enableTime&quot;:true}),picker.open()" name="birthday" type="text" id="birthday" placeholder="Y-m-d H:i" />
             HTML;
 
         $this->assertComponentRenders($expected, '<x-flat-pickr name="birthday"/>');
@@ -24,7 +24,7 @@ class FlatPickrTest extends ComponentTestCase
         $this->flashOld(['birthday' => '23/03/1989']);
 
         $expected = <<<'HTML'
-            <input x-data x-init="flatpickr($el, {&quot;dateFormat&quot;:&quot;Y-m-d H:i&quot;,&quot;altInput&quot;:true,&quot;enableTime&quot;:true})" name="birthday" type="text" id="birthday" placeholder="Y-m-d H:i" value="23/03/1989" />
+            <input x-data="{picker:null}" x-on:click="picker=picker||flatpickr($el, {&quot;dateFormat&quot;:&quot;Y-m-d H:i&quot;,&quot;altInput&quot;:true,&quot;enableTime&quot;:true}),picker.open()" name="birthday" type="text" id="birthday" placeholder="Y-m-d H:i" value="23/03/1989" />
             HTML;
 
         $this->assertComponentRenders($expected, '<x-flat-pickr name="birthday"/>');
