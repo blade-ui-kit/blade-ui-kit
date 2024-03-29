ceedc179baf100a138b9ac03f5425443a0e104ae
Dries Vints <dries@vints.be>
2024-03-29 16:00:53 +0100

    Update tests/Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -7,8 +7,6 @@ uses(ComponentTestCase::class)->in('Components');
 /**
  * Render the contents of the given Blade template string.
  *
- * @param  string  $template
- * @param  \Illuminate\Contracts\Support\Arrayable|array  $data
  * @return \Tests\TestView
  */
 function blade(string $template, array $data = [])
