eebe50d0336343726ac648ee54165b73c3028143
Dries Vints <dries@vints.be>
2024-04-03 16:03:03 +0200

    Update tests/Browser/DuskTestCase.php

--- tests/Browser/DuskTestCase.php
+++ tests/Browser/DuskTestCase.php
@@ -19,8 +19,6 @@ abstract class DuskTestCase extends \Orchestra\Testbench\Dusk\TestCase
      * Define environment setup.
      *
      * @param  Illuminate\Foundation\Application  $app
-     *
-     * @return void
      */
     protected function defineEnvironment($app): void
     {
