bf3790ae71a3157f3dbb9addebbb8e965aecdcb2
luttje <2738114+luttje@users.noreply.github.com>
2024-04-03 16:04:07 +0200

    Add e2e Dusk tests (#157)
    * e2e test for single component per page
    
    * Allow rendering multiple components on page in browser tests
    
    * Downgrade testbench to work with php 8.1
    
    * Change dependencies to support both php 8.1 and 8.2
    
    * Update composer.json
    
    * Update tests/Browser/ComponentConfig.php
    
    * Update tests/Browser/ComponentConfig.php
    
    * Update tests/Browser/ComponentConfig.php
    
    * Update tests/Browser/ComponentConfig.php
    
    * Update tests/Browser/DuskTestCase.php
    
    ---------
    
    Co-authored-by: Dries Vints <dries@vints.be>

---
+++ tests/Browser/DuskTestCase.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Browser;
+
+use BladeUIKit\BladeUIKitServiceProvider;
+use Livewire\LivewireServiceProvider;
+
+abstract class DuskTestCase extends \Orchestra\Testbench\Dusk\TestCase 
+{
+    protected function getPackageProviders($app)
+    {
+        return [
+            LivewireServiceProvider::class,
+            BladeUIKitServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  Illuminate\Foundation\Application  $app
+     */
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('app.key', 'base64:Hupx3yAySikrM2/edkZQNQHslgDWYfiBfCuSThJ5SK8=');
+
+        $app['config']->set('app.env', env('APP_ENV', 'testing'));
+        $app['config']->set('app.debug', env('APP_DEBUG', true));
+        $app['config']->set('app.url', $this->applicationBaseUrl());
+    }
+
+    /**
+     * Define web routes setup.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function defineWebRoutes($router)
+    {
+        $router->get('/blade-ui-kit/blade/{componentConfigs}', function ($componentConfigs) {
+            $html = ComponentConfig::deserializeComponentConfigsToHtml(
+                urldecode($componentConfigs)
+            );
+
+            return ComponentConfig::wrapComponentHtml($html);
+        })->name('blade-ui-kit.component');
+    }
+
+    /**
+     * Make a route to a page containing the specified Blade component(s).
+     * 
+     * @param ComponentConfig[]|ComponentConfig $componentConfig 
+     * @return string 
+     */
+    protected function makeComponentRoute($componentConfigs)
+    {
+        if (!is_array($componentConfigs)) {
+            $componentConfigs = [$componentConfigs];
+        }
+
+        $componentConfigs = urlencode(
+            ComponentConfig::serializeComponentsConfigs($componentConfigs)
+        );
+
+        return route('blade-ui-kit.component', compact('componentConfigs'), false);
+    }
+}
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,9 +1,11 @@
 <?php
 
+use Tests\Browser\DuskTestCase;
 use Tests\Components\ComponentTestCase;
 use Tests\TestView;
 
 uses(ComponentTestCase::class)->in('Components');
+uses(DuskTestCase::class)->in('Browser');
 
 function blade(string $template, array $data = []): TestView
 {
