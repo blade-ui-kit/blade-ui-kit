5b488fddd93ca9bcd2b1dc8830c097a0de404123
Alexandru Manase <malexandru699@gmail.com>
2024-04-04 16:13:38 +0300

    Make easyMDE and Trix compatible with Livewire 3 (#159)
    * do not load Alpine if Livewire has `auto_inject`
    
    * make easyMDE component compatible with Livewire 3
    
    * update snapshots
    
    * always run Alpine in testing environment
    
    * make trix component compatible with Livewire 3
    
    * update snapshots
    
    * cleanup
    
    * Update src/BladeUIKit.php
    
    * Update resources/views/components/editors/trix.blade.php
    
    * Update resources/views/components/editors/trix.blade.php
    
    ---------
    
    Co-authored-by: Dries Vints <dries@vints.be>

--- resources/views/components/editors/trix.blade.php
+++ resources/views/components/editors/trix.blade.php
@@ -1,4 +1,20 @@
-<div {{ $attributes }}>
+@php
+$model = $attributes->whereStartsWith('wire:model');
+$key = uniqid();
+@endphp
+
+<div {{ $attributes->whereDoesntStartWith('wire:model') }} wire:ignore>
     <input name="{{ $name }}" id="{{ $id }}" value="{{ old($name, $slot) }}" type="hidden">
-    <trix-editor input="{{ $id }}" class="{{ $styling }}"></trix-editor>
+
+    <trix-editor
+        @if ($model->first())
+            x-data
+            x-on:trix-change="$dispatch('input', event.target.value)"
+            x-ref="trix"
+            wire:key="{{ $key }}"
+            {{ $attributes->whereStartsWith('wire:model') }}
+        @endif
+        input="{{ $id }}"
+        class="{{ $styling }}"
+    ></trix-editor>
 </div>
