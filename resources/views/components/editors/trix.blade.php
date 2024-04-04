42721e611e56be44e6be5a7c3f2b947403cf3b71
Dries Vints <dries@vints.be>
2024-04-04 14:13:41 +0200

    Update resources/views/components/editors/trix.blade.php

--- resources/views/components/editors/trix.blade.php
+++ resources/views/components/editors/trix.blade.php
@@ -3,7 +3,6 @@ $model = $attributes->whereStartsWith('wire:model');
 $key = uniqid();
 @endphp
 
-
 <div {{ $attributes->whereDoesntStartWith('wire:model') }} wire:ignore>
     <input name="{{ $name }}" id="{{ $id }}" value="{{ old($name, $slot) }}" type="hidden">
 
