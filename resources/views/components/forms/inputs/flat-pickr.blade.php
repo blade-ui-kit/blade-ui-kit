c14318cc2c5312705e33dcd93bdc2f0541651a2c
Alex Manase <malexandru699@gmail.com>
2024-04-04 19:34:42 +0300

    make flat-pickr compatible with Livewire 3

--- resources/views/components/forms/inputs/flat-pickr.blade.php
+++ resources/views/components/forms/inputs/flat-pickr.blade.php
@@ -1,16 +1,18 @@
-<input
-    x-data="{
-        picker: null,
-    }"
-    x-init="$nextTick(() => { 
-        if (picker) return;
-            
-        picker = flatpickr($root, {{ $jsonOptions() }});
-    })"
-    name="{{ $name }}"
-    type="text"
-    id="{{ $id }}"
-    placeholder="{{ $placeholder }}"
-    @if($value) value="{{ $value }}" @endif
-    {{ $attributes }}
-/>
+<div wire:ignore>
+    <input
+        x-data="{
+            picker: null,
+        }"
+        x-init="$nextTick(() => { 
+            if (picker) return;
+                
+            picker = flatpickr($root, {{ $jsonOptions() }});
+        })"
+        name="{{ $name }}"
+        type="text"
+        id="{{ $id }}"
+        placeholder="{{ $placeholder }}"
+        @if($value) value="{{ $value }}" @endif
+        {{ $attributes }}
+    />
+</div>
