ab61e80a857a236aec2a4c889515319a1021873d
luttje <2738114+luttje@users.noreply.github.com>
2024-04-14 08:28:41 +0200

    Add e2e dusk test for color picker

--- tests/Browser/Forms/Inputs/ColorPickerTest.php
+++ tests/Browser/Forms/Inputs/ColorPickerTest.php
@@ -22,6 +22,7 @@ class ColorPickerTest extends DuskTestCase
                     new ComponentConfig('color-picker', [
                         'name' => 'color1',
                         'dusk' => 'color1',
+                        'x-on:buk:color-save' => 'window.test_render = `1 ${$event.detail.color}`',
                     ])
                 )
             )
@@ -30,7 +31,8 @@ class ColorPickerTest extends DuskTestCase
             ->waitFor('.pcr-palette')
             ->click('.pcr-palette')
             ->click('.pcr-save')
-            ->waitUntil("$inputElement.value !== ''");
+            ->waitUntil("$inputElement.value !== ''")
+            ->assertScript('return window.test_render === `1 ${' . $inputElement . '.value}`');
         });
     }
     
@@ -45,10 +47,12 @@ class ColorPickerTest extends DuskTestCase
                     new ComponentConfig('color-picker', [
                         'name' => 'color1',
                         'dusk' => 'color1',
+                        '@buk:color-change' => 'window.test_render_multiple1 = `1 ${$event.detail.color}`',
                     ]),
                     new ComponentConfig('color-picker', [
                         'name' => 'color2',
                         'dusk' => 'color2',
+                        '@buk:color-change' => 'window.test_render_multiple2 = `2 ${$event.detail.color}`',
                     ]),
                 ])
             )
@@ -59,12 +63,15 @@ class ColorPickerTest extends DuskTestCase
             ->click('.pcr-app .pcr-save')
             ->waitUntil("$inputElement1.value !== ''")
             ->click('@color1 button')
+            ->assertScript('return window.test_render_multiple1 === `1 ${' . $inputElement1 . '.value}`')
+            ->assertScript('return window.test_render_multiple2 === undefined')
             ->assertScript("return $inputElement2.value === ''")
             ->click('@color2 button')
             ->waitFor('.pcr-app:last-child .pcr-palette')
             ->click('.pcr-app:last-child .pcr-palette')
             ->click('.pcr-app:last-child .pcr-save')
-            ->waitUntil("$inputElement2.value !== ''");
+            ->waitUntil("$inputElement2.value !== ''")
+            ->assertScript('return window.test_render_multiple2 === `2 ${' . $inputElement2 . '.value}`');
         });
     }
 }
